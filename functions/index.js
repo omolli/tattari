e41d690676d2edc7b01d284b06a8c4b6d6ac13fd
Otso Ollikainen <otso.ollikainen@aalto.fi>
2019-08-29 15:09:59 +0300

    context.data.nice

--- functions/index.js
+++ functions/index.js
@@ -170,11 +170,10 @@ app.intent('1_3bossresponse', (conv, {response}) => {
     const audiourl = host+ '110.mp3';
     const answ = response;
     conv.data.previous = ['1_6event',answ,'int1_5'];
+    conv.contexts.data.nice = false;
     if (answ === 'one') {
-      conv.contexts.set('kerrov', 5, {nice:true});
+      conv.contexts.data.nice = true;
       conv.data.vpoints++;
-    } else {
-      conv.contexts.set('kerrov', 5, {nice: false});
     }
     conv.ask(Utils.playSimple(audiourl));
   });
@@ -184,15 +183,13 @@ app.intent('1_3bossresponse', (conv, {response}) => {
     const answ = response;
     conv.data.previous = ['1_7event',answ, 'int1_6'];
     // Tähän failsafe?
-    const param = conv.contexts.get('kerrov').parameters;
+    const param = conv.contexts.data.nice;
     var audiourl = host + '111T.mp3';
     var urlc = '';
-    if (param['nice'] === true) {
-      conv.contexts.set('kerrov', 5, {nice: true});
+    if (param) {
       urlc = host + '111';
     } else {
       urlc = host + '112';
-      conv.contexts.set('kerrov', 5, {nice: false});
     }
     if (answ === 'one') {
       audiourl = urlc + 'T.mp3';
